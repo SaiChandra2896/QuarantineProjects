51d71da891d821e3961b0aa03300a3a87826903d
SaiChandra2896 <saichandra2896@gmail.com>
2020-04-16 14:20:52 +0530

    add check for length and passwords match

--- FormValidator/script.js
+++ FormValidator/script.js
@@ -18,9 +18,13 @@ function showSuccess(input) {
 }
 
 //check valid email
-function isValidEmail(email) {
+function isValidEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (re.test(input.value.trim())) {
+        showSuccess(input)
+    } else {
+        showError(input, 'Email is not valid')
+    }
 }
 
 //check required feilds
@@ -34,9 +38,27 @@ function checkRequired(inputArr) {
     });
 };
 
+//check input length
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFeildName(input)} must be atleast ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFeildName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
 //get feild name 
 function getFeildName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+}
+
+//ckeck passwords match
+function checkpasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match')
+    }
 }
 
 //event listeners
@@ -44,4 +66,8 @@ form.addEventListener('submit', function (e) {
     e.preventDefault();
 
     checkRequired([username, email, password, password2]);
+    checkLength(username, 4, 15);
+    checkLength(password, 6, 20);
+    isValidEmail(email);
+    checkpasswordsMatch(password, password2);
 });
