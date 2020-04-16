77e35aeabb36d1056e79d64f2d4a8d4a5f54eeb1
SaiChandra2896 <saichandra2896@gmail.com>
2020-04-16 13:59:11 +0530

    add getfeildname function to make uppercase first letter in id

--- FormValidator/script.js
+++ FormValidator/script.js
@@ -27,12 +27,17 @@ function isValidEmail(email) {
 function checkRequired(inputArr) {
     inputArr.forEach(function (input) {
         if (input.value.trim() === '') {
-            showError(input, `${input.id} is required`);
+            showError(input, `${getFeildName(input)} is required`);
         } else {
             showSuccess(input);
         }
     });
 };
+
+//get feild name 
+function getFeildName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+}
 
 //event listeners
 form.addEventListener('submit', function (e) {
