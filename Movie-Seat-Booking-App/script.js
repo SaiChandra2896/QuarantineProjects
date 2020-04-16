4b9d86baade5c0724bb9507de1bcf4bc646f1559
SaiChandra2896 <saichandra2896@gmail.com>
2020-04-16 19:45:02 +0530

    add javascript logic

--- Movie-Seat-Booking-App/script.js
+++ Movie-Seat-Booking-App/script.js
@@ -3,3 +3,28 @@ const container = document.querySelector('.container');
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+let ticketPrice = parseInt(movieSelect.value);
+
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const seatsCount = selectedSeats.length;
+
+    count.innerText = seatsCount;
+    total.innerText = seatsCount * ticketPrice;
+}
+
+//movie select event
+movieSelect.addEventListener('change', e => {
+    ticketPrice = parseInt(e.target.value);
+    updateSelectedCount();
+})
+
+//select seats event
+container.addEventListener('click', (e) => {
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected');
+        updateSelectedCount();
+    }
+});
